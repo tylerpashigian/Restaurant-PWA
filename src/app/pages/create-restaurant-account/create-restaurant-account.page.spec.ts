c21d1940f4167016459f94d0f24834961db8d4b6
Tyler Pashigian <tylerpashigian@gmail.com>
2020-10-20 07:56:45 -0500

    Adding required modules to resolve errors when executing tests

--- src/app/pages/create-restaurant-account/create-restaurant-account.page.spec.ts
+++ src/app/pages/create-restaurant-account/create-restaurant-account.page.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CreateRestaurantAccountPage } from './create-restaurant-account.page';
 
@@ -10,7 +13,12 @@ describe('CreateRestaurantAccountPage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateRestaurantAccountPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        FormsModule,
+        IonicModule.forRoot(),
+        ReactiveFormsModule,
+        RouterTestingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreateRestaurantAccountPage);
--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomePage } from './home.page';
 
@@ -10,7 +13,12 @@ describe('HomePage', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        FormsModule,
+        IonicModule.forRoot(),
+        ReactiveFormsModule,
+        RouterTestingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
