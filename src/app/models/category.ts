ce582f6afb47236e8a1330921cb314ac3f634f6c
Tyler Pashigian <tylerpashigian@gmail.com>
2021-03-23 08:23:03 -0500

    Adding TODO comment

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -3,6 +3,7 @@ import { MenuItem } from './menuItem';
 export interface Category {
   id?: string,
   endTime?: string;
+  // TODO: Remove this and convert all menu items references to use a map (menuItemsMap)
   menuItems?: MenuItem[];
   menuItemMap?: { [id: string]: MenuItem }
   title: string;
